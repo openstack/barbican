aeaae4fbdc8d1ffa6431931540496958010736cf
Jarret Raim <jarito@gmail.com>
2013-02-17 14:45:13 -0600

    Authenciation & Admin
    Added flask-login for authentication of the web portion. Included flask-admin rather than writing our own views for the various models.

--- barbican.py
+++ barbican.py
@@ -13,25 +13,65 @@
     :license: Apache 2.0, see LICENSE for details
 """
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, flash, request
+from flask.ext.admin import Admin
+from flask.ext.admin.contrib.sqlamodel import ModelView
+from flask.ext import login, wtf
+from flask.ext.login import login_user
 from barbican_api import api
 from database import db_session, init_db
 from models import User
 
 
 app = Flask(__name__)
+app.secret_key = '79f9823f1f0---DEVELOPMENT---c46cebdd1c8f3d0742e02'
 app.register_blueprint(api)
+
+admin = Admin(app, name="Barbican Admin")
+admin.add_view(ModelView(User, db_session))
+
+login_manager = login.LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 
 @app.route("/")
+@login.login_required
 def hello():
     return "Hello world!"
 
 
-@app.route('/users')
-def users_list():
-    users = User.query.all()
-    return render_template('users.html', users=users)
+#
+#   Login forms
+#
+class LoginForm(wtf.Form):
+    login = wtf.TextField(validators=[wtf.required()])
+    password = wtf.PasswordField(validators=[wtf.required()])
+
+    def validate_login(self, field):
+        user = self.get_user()
+        if user is None or user.password != self.password.data:
+            raise wtf.ValidationError('Invalid username or credentials.')
+
+    def get_user(self):
+        return User.query.filter_by(name=self.login.data).first()
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm(request.form)
+    if form.validate_on_submit():
+        user = form.get_user()
+        login_user(user)
+        flash('Logged in successfully.')
+        return redirect('/admin/')
+
+    return render_template("login.html", form=form)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 
 @app.teardown_request
@@ -41,5 +81,6 @@ def shutdown_session(exception=None):
 
 if __name__ == '__main__':
     if not os.path.exists('/tmp/barbican.db'):
+        app.logger.info('No database detected at /tmp/barbican.db. Creating one and the admin user.')
         init_db()
     app.run(debug=True)
--- models.py
+++ models.py
@@ -17,10 +17,24 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True)
     email = Column(String(120), unique=True)
+    password = Column(String(50))
 
-    def __init__(self, name=None, email=None):
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
+
+    def __init__(self, name=None, email=None, password=None):
         self.name = name
         self.email = email
+        self.password = password
 
     def __repr__(self):
         return '<User %r>' % self.name
