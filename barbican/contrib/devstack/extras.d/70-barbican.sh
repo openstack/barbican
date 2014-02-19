eb3e7ef50bcdd8647a646c1f2d3198744e798ca1
Chad Lung <chad.lung@gmail.com>
2014-02-19 00:33:17 -0600

    Adding DevStack Support
    Change-Id: Iacdf5d3c78a613437de3e472e8e1a781fe3db5a4

---
+++ barbican/contrib/devstack/extras.d/70-barbican.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# DevStack extras script to install Barbican
+
+if is_service_enabled barbican; then
+    if [[ "$1" == "source" ]]; then
+        # Initial source
+        source $TOP_DIR/lib/barbican
+    elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+        echo_summary "Installing Barbican"
+        install_barbicanclient
+        install_barbican
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        echo_summary "Configuring Barbican"
+        configure_barbican
+        configure_barbicanclient
+
+        if is_service_enabled key; then
+            create_barbican_accounts
+        fi
+
+    elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
+        echo_summary "Initializing Barbican"
+        init_barbican
+        start_barbican
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+        stop_barbican
+    fi
+fi
---
+++ barbican/functionaltests/run_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# How many seconds to wait for the API to be responding before giving up
+API_RESPONDING_TIMEOUT=20
+
+if ! timeout ${API_RESPONDING_TIMEOUT} sh -c "while ! curl -s -o /dev/null http://127.0.0.1:9311/ ; do sleep 1; done"; then
+    echo "API failed to respond within ${API_RESPONDING_TIMEOUT} seconds"
+    exit 1
+fi
+
+# Where tempest code lives
+TEMPEST_DIR=${TEMPEST_DIR:-/opt/stack/new/tempest}
+
+# Add tempest source tree to PYTHONPATH
+export PYTHONPATH=$PYTHONPATH:$TEMPEST_DIR
+
+nosetests -v .
