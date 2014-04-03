0465b850877244bb6d1f4b091cce57452c91f2a1
Ade Lee <alee@redhat.com>
2014-04-03 14:22:40 -0400

    Add Dogtag crypto plugin.
    This patch adds an initial version of the Dogtag plugin
    that implements the decrypt(), encrypt() and generate()
    functions.  In this version, we will create and initialize
    an NSS database on the server to do crypto operations
    within the Dogtag DRM python client.  This allows us to
    decrypt() and encrypt() secrets without passing in wrapping
    keys.
    
    When the barbican-client and the Barbican server API have
    been changed to pass through the wrapping keys, this plugin
    will need to enhanced to make the relevant DRM client calls.
    
    This plugin has been tested by running the barbican server
    within a virtual environment.  There are several steps
    needed to get things configured, which are detailed in the
    blueprint.
    
    Added unit tests.  These unit tests will be skipped if the
    dogtag dependencies are not present to import.
    
    Change-Id: Ib53bf3df5a65af7ac602ec7f7895e9c723c36c06
    Implements: blueprint dogtag-plugin

---
+++ barbican/tests/crypto/test_dogtag_crypto.py
@@ -0,0 +1,164 @@
+# Copyright (c) 2014 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import mock
+import os
+import tempfile
+import testtools
+
+try:
+    from barbican.crypto import plugin as plugin_import
+    from barbican.crypto.dogtag_crypto import DogtagCryptoPlugin
+    from barbican.model import models
+    imports_ok = True
+except:
+    # dogtag imports probably not available
+    imports_ok = False
+
+
+class WhenTestingDogtagCryptoPlugin(testtools.TestCase):
+
+    def setUp(self):
+        super(WhenTestingDogtagCryptoPlugin, self).setUp()
+        if not imports_ok:
+            return
+
+        self.keyclient_mock = mock.MagicMock(name="KeyClient mock")
+        self.patcher = mock.patch('pki.cryptoutil.NSSCryptoUtil')
+        self.patcher.start()
+
+        # create nss db for test only
+        self.nss_dir = tempfile.mkdtemp()
+
+        self.cfg_mock = mock.MagicMock(name='config mock')
+        self.cfg_mock.dogtag_crypto_plugin = mock.MagicMock(
+            nss_db_path=self.nss_dir)
+        self.plugin = DogtagCryptoPlugin(self.cfg_mock)
+        self.plugin.keyclient = self.keyclient_mock
+
+    def tearDown(self):
+        super(WhenTestingDogtagCryptoPlugin, self).tearDown()
+        if not imports_ok:
+            return
+        self.patcher.stop()
+        os.rmdir(self.nss_dir)
+
+    def test_generate(self):
+        if not imports_ok:
+            self.skipTest("Dogtag imports not available")
+        secret = models.Secret()
+        secret.bit_length = 128
+        secret.algorithm = "AES"
+        generate_dto = plugin_import.GenerateDTO(
+            plugin_import.PluginSupportTypes.SYMMETRIC_KEY_GENERATION,
+            secret.algorithm,
+            secret.bit_length,
+            None)
+        _encrypted, _kek_ext = self.plugin.generate(
+            generate_dto,
+            mock.MagicMock(),
+            mock.MagicMock()
+        )
+
+        self.keyclient_mock.generate_symmetric_key.assert_called_once_with(
+            mock.ANY,
+            secret.algorithm.upper(),
+            secret.bit_length,
+            mock.ANY)
+
+    def test_raises_error_with_no_pem_path(self):
+        if not imports_ok:
+            self.skipTest("Dogtag imports not available")
+        m = mock.MagicMock()
+        m.dogtag_crypto_plugin = mock.MagicMock(pem_path=None)
+        self.assertRaises(
+            ValueError,
+            DogtagCryptoPlugin,
+            m,
+        )
+
+    def test_raises_error_with_no_pem_password(self):
+        if not imports_ok:
+            self.skipTest("Dogtag imports not available")
+        m = mock.MagicMock()
+        m.dogtag_crypto_plugin = mock.MagicMock(pem_password=None)
+        self.assertRaises(
+            ValueError,
+            DogtagCryptoPlugin,
+            m,
+        )
+
+    def test_raises_error_with_no_nss_password(self):
+        if not imports_ok:
+            self.skipTest("Dogtag imports not available")
+        m = mock.MagicMock()
+        m.dogtag_crypto_plugin = mock.MagicMock(nss_password=None)
+        self.assertRaises(
+            ValueError,
+            DogtagCryptoPlugin,
+            m,
+        )
+
+    def test_encrypt(self):
+        if not imports_ok:
+            self.skipTest("Dogtag imports not available")
+        payload = 'encrypt me!!'
+        encrypt_dto = plugin_import.EncryptDTO(payload)
+        _cyphertext, _kek_meta_extended = self.plugin.encrypt(encrypt_dto,
+                                                              mock.MagicMock(),
+                                                              mock.MagicMock())
+        self.keyclient_mock.archive_key.assert_called_once_with(
+            mock.ANY,
+            "passPhrase",
+            payload,
+            key_algorithm=None,
+            key_size=None)
+
+    def test_decrypt(self):
+        if not imports_ok:
+            self.skipTest("Dogtag imports not available")
+        key_id = 'key1'
+        decrypt_dto = plugin_import.DecryptDTO(key_id)
+        self.plugin.decrypt(decrypt_dto,
+                            mock.MagicMock(),
+                            mock.MagicMock(),
+                            mock.MagicMock())
+
+        self.keyclient_mock.retrieve_key.assert_called_once_with(key_id)
+
+    def test_supports_encrypt_decrypt(self):
+        if not imports_ok:
+            self.skipTest("Dogtag imports not available")
+        self.assertTrue(
+            self.plugin.supports(
+                plugin_import.PluginSupportTypes.ENCRYPT_DECRYPT
+            )
+        )
+
+    def test_supports_symmetric_key_generation(self):
+        if not imports_ok:
+            self.skipTest("Dogtag imports not available")
+        self.assertTrue(
+            self.plugin.supports(
+                plugin_import.PluginSupportTypes.SYMMETRIC_KEY_GENERATION
+            )
+        )
+
+    def test_does_not_support_unknown_type(self):
+        if not imports_ok:
+            self.skipTest("Dogtag imports not available")
+        self.assertFalse(
+            self.plugin.supports("SOMETHING_RANDOM")
+        )
