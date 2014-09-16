6bb4453cf3b91866871d275abb4783bce51e5bd6
Chelsea Winfree <chelsea.winfree@gmail.com>
2014-09-16 12:06:43 -0500

    Adding sub-status and message to orders
    Adding the ability to view sub-status and
    messages for orders. Added new Alembic
    Version also. Increasing Unit test coverage.
    
    Implements: blueprint add-ssl-ca-support
    Change-Id: I6c3c2cc840f34dd5206b165c94daf0fc1691cfe7

---
+++ barbican/model/migration/alembic_migrations/versions/2843d6469f25_add_sub_status_info_for_orders.py
@@ -0,0 +1,25 @@
+"""add sub status info for orders
+
+Revision ID: 2843d6469f25
+Revises: 2ab3f5371bde
+Create Date: 2014-09-16 12:31:15.181380
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '2843d6469f25'
+down_revision = '2ab3f5371bde'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column('orders', sa.Column('sub_status', sa.String(length=36), nullable=True))
+    op.add_column('orders', sa.Column('sub_status_message', sa.String(length=255), nullable=True))
+
+
+
+def downgrade():
+    op.drop_column('orders', 'sub_status')
+    op.drop_column('orders', 'sub_status_message')
