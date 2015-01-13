b984aadaf4655c962fffa25e7612d602bb0876b5
Tim Kelsey <tim.kelsey@hp.com>
2015-01-13 15:52:21 +0000

    Checking the certificate file has reasonable permissions
    - Note that these checks assume a POSIX complient system
    
    Change-Id: I7f2e32051b34819f83ffecdf2b520a5a1d553efb

--- barbican/plugin/kmip_secret_store.py
+++ barbican/plugin/kmip_secret_store.py
@@ -20,6 +20,8 @@ An implementation of the SecretStore that uses the KMIP backend.
 from kmip.services import kmip_client
 
 import base64
+import os
+import stat
 
 from kmip.core import enums
 from kmip.core.factories import attributes
@@ -110,7 +112,8 @@ class KMIPSecretStore(ss.SecretStoreBase):
                 enums.CryptographicAlgorithm.TRIPLE_DES}
         }
 
-        # TODO(tkelsey): check the certificate file has good permissions
+        if conf.kmip_plugin.keyfile is not None:
+            self._check_keyfile_permissions(conf.kmip_plugin.keyfile)
 
         credential_type = credentials.CredentialType.USERNAME_AND_PASSWORD
         credential_value = {'Username': conf.kmip_plugin.username,
@@ -510,3 +513,16 @@ class KMIPSecretStore(ss.SecretStoreBase):
         )
         LOG.debug("ERROR from KMIP server: %s", msg)
         raise ss.SecretGeneralException(msg)
+
+    def _check_keyfile_permissions(self, path):
+        """Check that file has permissions appropriate for a sensitive key
+
+        Key files are extremely sensitive, they should be owned by the user
+        who they relate to. They should be readable only (to avoid accidental
+        changes). They should not be readable or writeable by any other user.
+        """
+        expected = (stat.S_IRUSR | stat.S_IFREG)  # 0o100400
+        st = os.stat(path)
+        if st.st_mode != expected:
+            LOG.warning(u._('Poor key file permissions found, recommend 400 '
+                            'for path: {file_path}').format(file_path=path))
