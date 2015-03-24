3e55dde216c48c61c5faf8d020df2c4bb569d8d5
John Vrbanac <john.vrbanac@rackspace.com>
2015-03-23 23:37:28 -0500

    Removing unused TimeKeeper class
    The TimeKeeper class was an old piece of code that was added back at the
    beginning of the project, but hasn't been used for over a year.
    
    I'm considering this dead code and removing.
    
    Change-Id: Ic8b9a94715b17d49e2460cd942d00171cc1ec8d3

--- barbican/common/utils.py
+++ barbican/common/utils.py
@@ -18,7 +18,6 @@ Common utilities for Barbican.
 """
 
 import mimetypes
-import time
 import uuid
 
 from oslo_config import cfg
@@ -129,44 +128,6 @@ def generate_fullname_for(instance):
     if module is None or module == "__builtin__":
         return class_name
     return "{module}.{class_name}".format(module=module, class_name=class_name)
-
-
-class TimeKeeper(object):
-    """TimeKeeper object
-
-    Keeps track of elapsed times and then allows for dumping a summary to
-    logs. This class can be used to profile a method as a fine grain level.
-    """
-
-    def __init__(self, name, logger=None):
-        self.logger = logger or getLogger(__name__)
-        self.name = name
-        self.time_start = time.time()
-        self.time_last = self.time_start
-        self.elapsed = []
-
-    def mark(self, note=None):
-        """Mark a moment in time, with an optional note
-
-        :param note: Optional note about what is occurring at this time
-        """
-        time_curr = time.time()
-        self.elapsed.append((time_curr, time_curr - self.time_last, note))
-        self.time_last = time_curr
-
-    def dump(self):
-        """Dump the elapsed time(s) to log."""
-        self.logger.debug("Timing output for '{0}'".format(self.name))
-        for timec, timed, note in self.elapsed:
-            self.logger.debug("    time current/elapsed/notes:"
-                              "{0:.3f}/{1:.0f}/{2}".format(timec,
-                                                           timed * 1000.,
-                                                           note))
-        time_current = time.time()
-        total_elapsed = time_current - self.time_start
-        self.logger.debug("    Final time/elapsed:"
-                          "{0:.3f}/{1:.0f}".format(time_current,
-                                                   total_elapsed * 1000.))
 
 
 def generate_uuid():
