e9df5cb130fa1f569ffc0fa2c5f942dece3a36f3
Dave McCowan <dmccowan@cisco.com>
2015-05-08 00:13:32 -0400

    Add Functional Tests for ACLs Using Multiple Users
    Using the new multi-user support, add functional tests to exercise
    secret and container GETs for different users and different configured
    ACLs.
    
    Six users with four roles in two projects are used:
       In Project A: Admin, Creator, Observer, Auditor
       In Project B: Admin, Observer
    
    Five ACL scenarios are tested:
       No ACL: default
       RBAC Only: project-acess is True, ACL is empty
       Creator Only: project-access is False, ACL is empty
       ACL Only: project-access is False, ACL has a user
       RBAC + ACL: project-access is TruA, ACL has a user
    
    Get Secrets and Get Containers are tested for each user in each scenario.
    
    Implements: blueprint multi-user-functional-tests
    Relates-to: blueprint add-run-as-for-functional-tests
    
    Change-Id: Iff82ad90b36f9962fadd0578a9a01ec16c111099

---
+++ functionaltests/api/v1/behaviors/acl_behaviors.py
@@ -0,0 +1,83 @@
+"""
+Copyright 2015 Cisco Systems
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from functionaltests.api.v1.behaviors import base_behaviors
+from functionaltests.api.v1.models import acl_models
+
+
+class AclBehaviors(base_behaviors.BaseBehaviors):
+
+    def create_acl(self, entity_ref, model, extra_headers=None,
+                   user_name=None):
+        """Create an acl from the data in the model.
+
+        :param entity_ref: ref of secret or container for acl
+        :param model: The metadata used to create the acl
+        :param extra_headers: Headers used to create the acl
+        :param user_name: The user name used to create the acl
+        :return: the response from the PUT request
+        """
+
+        acl_ref = '{0}/acl'.format(entity_ref)
+        resp = self.client.put(acl_ref, request_model=model,
+                               extra_headers=extra_headers,
+                               user_name=user_name)
+
+        self.created_entities.append((acl_ref, user_name))
+        return resp
+
+    def get_acl(self, acl_ref, extra_headers=None, user_name=None):
+        """Handles getting a single acl
+
+        :param acl_ref: Reference to the acl to be retrieved
+        :param extra_headers: Headers used to get the acl
+        :param user_name: The user name used to get the acl
+
+        :return: The response of the GET.
+        """
+        resp = self.client.get(
+            acl_ref, response_model_type=acl_models.aclModel,
+            user_name=user_name)
+
+        return resp
+
+    def delete_acl(self, acl_ref, extra_headers=None,
+                   expected_fail=False, user_name=None):
+        """Handles deleting an acl.
+
+        :param acl_ref: Reference of the acl to be deleted
+        :param extra_headers: Any additional headers needed.
+        :param expected_fail: If there is a negative test, this should be
+            marked true if you are trying to delete an acl that does
+            not exist.
+        :param user_name: The user name used to delete the acl
+
+        :return: Response of the delete.
+        """
+        resp = self.client.delete(acl_ref, extra_headers, user_name=user_name)
+
+        if not expected_fail:
+            for item in self.created_entities:
+                if item[0] == acl_ref:
+                    self.created_entities.remove(item)
+
+        return resp
+
+    def delete_all_created_acls(self):
+        """Delete all of the acls that we have created."""
+        entities = list(self.created_entities)
+        for (acl_ref, user_name) in entities:
+            self.delete_acl(acl_ref, user_name=user_name)
