131c34e8824e3cecee6a5759c1f5f31de582c471
Juan Antonio Osorio Robles <juan.osorio.robles@ericsson.com>
2015-06-22 17:05:07 +0300

    Fill project_id in secrets where needed
    This CR introduces a migration script that fills the project_id in the
    secrets that have it set to None; Which will be the case in the secrets
    created before the project_id column was introduced.
    
    On the other hand, this also makes the project_id column in the secrets
    table no longer nullable.
    
    blueprint data-remove-tenant-secret-assoc
    
    Change-Id: Iede2d2fcf6487ec3ef1b8d29a6000e2340d4c828

---
+++ barbican/model/migration/alembic_migrations/versions/161f8aceb687_fill_project_id_to_secrets_where_missing.py
@@ -0,0 +1,47 @@
+"""fill project_id to secrets where missing
+
+Revision ID: 161f8aceb687
+Revises: 1bc885808c76
+Create Date: 2015-06-22 15:58:03.131256
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '161f8aceb687'
+down_revision = '1bc885808c76'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def _get_database_metadata():
+    con = op.get_bind()
+    metadata = sa.MetaData(bind=con)
+    metadata.reflect()
+    return metadata
+
+
+def upgrade():
+    metadata = _get_database_metadata()
+
+    # Get relevant tables
+    secrets = metadata.tables['secrets']
+    project_secret = metadata.tables['project_secret']
+
+    # Add project_id to the secrets
+    op.execute(secrets.update().
+               values({'project_id': project_secret.c.project_id}).
+               where(secrets.c.id == project_secret.c.secret_id).
+               where(secrets.c.project_id == None)
+               )
+
+    # make project_id no longer nullable
+    op.alter_column('secrets', 'project_id', nullable=False)
+
+
+def downgrade():
+    metadata = _get_database_metadata()
+    secrets = metadata.tables['secrets']
+    op.alter_column('secrets', 'project_id', nullable=True)
+    op.execute(secrets.update().
+               values({'project_id': None}))
