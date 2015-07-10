486a4e2a2b4d926e9b0e1bfb9e8776b3407031df
Charles Neill <charles.neill@rackspace.com>
2015-07-10 11:28:40 -0500

    Unauthed tests for Secret & Container ACLs
    These tests will attempt CRUD operations on Secret and Container ACLs
    without valid tokens. They should all return HTTP 401s. Also added
    PATCH support to the common client, and use_auth parameters to ACL
    behaviors.
    
    Change-Id: Ie753b6605ae81202a4bb473dd6f9101919f4eda5

--- functionaltests/api/v1/behaviors/acl_behaviors.py
+++ functionaltests/api/v1/behaviors/acl_behaviors.py
@@ -21,37 +21,40 @@ from functionaltests.api.v1.models import acl_models
 class AclBehaviors(base_behaviors.BaseBehaviors):
 
     def create_acl(self, entity_ref, model, extra_headers=None,
-                   user_name=None):
+                   use_auth=True, user_name=None):
         """Create an acl from the data in the model.
 
         :param entity_ref: ref of secret or container for acl
         :param model: The metadata used to create the acl
         :param extra_headers: Headers used to create the acl
+        :param use_auth: Boolean for whether to send authentication headers
         :param user_name: The user name used to create the acl
+
         :return: the response from the PUT request
         """
 
         acl_ref = '{0}/acl'.format(entity_ref)
         resp = self.client.put(acl_ref, request_model=model,
-                               extra_headers=extra_headers,
+                               extra_headers=extra_headers, use_auth=use_auth,
                                user_name=user_name)
 
         self.created_entities.append((acl_ref, user_name))
         return resp
 
-    def get_acl(self, acl_ref, extra_headers=None, user_name=None):
+    def get_acl(self, acl_ref, extra_headers=None, use_auth=True,
+                user_name=None):
         """Handles getting a single acl
 
         :param acl_ref: Reference to the acl to be retrieved
         :param extra_headers: Headers used to get the acl
+        :param use_auth: Boolean for whether to send authentication headers
         :param user_name: The user name used to get the acl
 
         :return: The response of the GET.
         """
         resp = self.client.get(
             acl_ref, response_model_type=acl_models.AclModel,
-            user_name=user_name)
-
+            use_auth=use_auth, user_name=user_name)
         return resp
 
     def update_acl(self, acl_ref, model, extra_headers=None,
@@ -74,7 +77,7 @@ class AclBehaviors(base_behaviors.BaseBehaviors):
         return resp
 
     def delete_acl(self, acl_ref, extra_headers=None,
-                   expected_fail=False, user_name=None):
+                   expected_fail=False, use_auth=True, user_name=None):
         """Handles deleting an acl.
 
         :param acl_ref: Reference of the acl to be deleted
@@ -82,11 +85,13 @@ class AclBehaviors(base_behaviors.BaseBehaviors):
         :param expected_fail: If there is a negative test, this should be
             marked true if you are trying to delete an acl that does
             not exist.
+        :param use_auth: Boolean for whether to send authentication headers
         :param user_name: The user name used to delete the acl
 
         :return: Response of the delete.
         """
-        resp = self.client.delete(acl_ref, extra_headers, user_name=user_name)
+        resp = self.client.delete(acl_ref, extra_headers, use_auth=use_auth,
+                                  user_name=user_name)
 
         if not expected_fail:
             for item in self.created_entities:
