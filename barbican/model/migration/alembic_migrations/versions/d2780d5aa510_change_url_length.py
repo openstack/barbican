ffa09b9e145e73dff312c1a6b535d87050bb7e8c
Christopher Solis <cnsolis@us.ibm.com>
2016-03-10 15:39:56 -0600

    Changes max string length for URL to 255
    Since the barbican database character set is set to utf-8 in devstack,
    the max value for a string is 255. Having more than 255 throws an error.
    Fixing this bug will also allow us to use the barbican-manage command
    in the devstack gate to test migration so errors like this do not
    go unchecked in the future.
    
    Change-Id: I7ea320f1640977044c8058f682b43b42e7a5e2e2
    Closes-Bug: 1528774

---
+++ barbican/model/migration/alembic_migrations/versions/d2780d5aa510_change_url_length.py
@@ -0,0 +1,22 @@
+"""change_url_length
+
+Revision ID: d2780d5aa510
+Revises: dce488646127
+Create Date: 2016-03-11 09:39:32.593231
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'd2780d5aa510'
+down_revision = 'dce488646127'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.alter_column(
+        'ContainerConsumerMetadatum',
+        'URL',
+        type_=sa.String(length=255)
+    )
